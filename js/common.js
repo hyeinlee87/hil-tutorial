28bc5c2c627c7fe372498b72404bc6ce004d7e42
hyeinlee87 <60733570+hyeinlee87@users.noreply.github.com>
2020-09-07 00:50:24 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,17 @@
+
+
+// swiper
+
+var leaderwiper = new Swiper('.strength-leadership .swiper-container', {
+	 spaceBetween: 20,
+	  slidesPerView: 'auto',
+	  centeredSlides: false,
+	  slideToClickedSlide: true,
+	  scrollbar: {
+		el: $('.swiper-scrollbar'),
+		draggable: true
+		}
+});
+
+
+
